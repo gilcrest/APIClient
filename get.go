f6c43f80aeebbf5253f16d8cfd675e29bb476fea
Dan Gillis <gilcrest@gmail.com>
2019-01-10 12:15:47 -0500

    Add ViaClientNum

--- get.go
+++ get.go
@@ -48,3 +48,38 @@ func ViaServerToken(ctx context.Context, tx *sql.Tx) (*Client, error) {
 		return client, nil
 	}
 }
+
+// ViaClientNum fetches a client from the database using the client's
+// Unique Number
+func ViaClientNum(ctx context.Context, tx *sql.Tx, clientNum int) (*Client, error) {
+	const op errors.Op = "apiclient/ViaClientNum"
+
+	client := new(Client)
+	err := tx.QueryRowContext(ctx, `SELECT client_num,
+										  client_id,
+										  client_name,
+										  server_token,
+										  homepage_url,
+										  app_description,
+										  redirect_uri,
+										  client_secret,
+										  primary_username,
+										  create_client_num,
+										  create_timestamp,
+										  modify_client_num,
+										  modify_timestamp
+									 FROM auth.client
+									WHERE client_num = $1`, clientNum).
+		Scan(&client.Number, &client.ID, &client.Name,
+			&client.ServerToken, &client.HomeURL, &client.Description, &client.RedirectURI, &client.Secret,
+			&client.PrimaryUserID, &client.CreateClientNumber, &client.CreateTimestamp,
+			&client.UpdateClientNumber, &client.UpdateTimestamp)
+	switch {
+	case err == sql.ErrNoRows:
+		return nil, errors.E(op, fmt.Errorf("No client with number %d", clientNum))
+	case err != nil:
+		return nil, errors.E(op, err)
+	default:
+		return client, nil
+	}
+}
